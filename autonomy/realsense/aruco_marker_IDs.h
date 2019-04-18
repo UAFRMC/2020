18876628ef447cff99a70fed68b9494a58e52c7e
Dr. Orion Lawlor <lawlor@alaska.edu>
2019-04-17 23:06:50 -0800

    Added front and back marker plates, with 18cm and 5cm markers.

--- autonomy/realsense/aruco_marker_IDs.h
+++ autonomy/realsense/aruco_marker_IDs.h
@@ -7,8 +7,9 @@ struct marker_info_t {
 	int side; // side of the robot (0==right side, 1==left side, 2==back side, 3==front side)
 	vec3 shift; // marker-relative cm translation to robot origin, in meters from center of pattern	
 };
-
-#define robot_side_vec(x) vec3(x,66,0)
+#define robot_frontback 33  // half the X front-back length of robot
+#define robot_side2side 66  // half the Y side-side width of robot
+#define robot_side_vec(x) vec3(x,robot_side2side,0)
 
 const static marker_info_t marker_info[]={
 	{-1,14.5}, // fallback default case
@@ -18,6 +19,15 @@ const static marker_info_t marker_info[]={
 	{14, 14.5, 0, -robot_side_vec(0) }, // cat
 	{25, 10.0, 0, -robot_side_vec(-15) }, // bleachers 
 	{16,  4.0, 0, -robot_side_vec(-4) }, // elipsis (on tiny 5cm test plate)
+	
+	{ 6, 18.0, 2, -vec3(-robot_frontback,robot_side2side-16.5,10) }, // back right plate
+	{ 7,  5.0, 2, -vec3(-robot_frontback,robot_side2side-4.5,4.5) }, 
+  
+	{26, 18.0, 2, -vec3(robot_frontback,robot_side2side-16.5,10) }, // front right plate
+	{27,  5.0, 2, -vec3(robot_frontback,robot_side2side-4.5,4.5) }, 
+	
+	
+	
 };
 
 // Look up the calibration parameters for this marker
