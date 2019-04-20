c812ee17ed2f7f83bba488dbbd2df22fbde72782
Dr. Lawlor's Pi3 <lawlor@alaska.edu>
2019-04-19 18:58:23 -0800

    Updated marker locations for as-built on existing marker panel.

--- autonomy/realsense/aruco_marker_IDs.h
+++ autonomy/realsense/aruco_marker_IDs.h
@@ -7,26 +7,24 @@ struct marker_info_t {
 	int side; // side of the robot (0==right side, 1==left side, 2==back side, 3==front side)
 	vec3 shift; // marker-relative cm translation to robot origin, in meters from center of pattern	
 };
-#define robot_frontback 33  // half the X front-back length of robot
-#define robot_side2side 66  // half the Y side-side width of robot
-#define robot_side_vec(x) vec3(x,robot_side2side,0)
+#define robot_frontback 37 // 33  // half the X front-back length of robot
+#define robot_side2side 70 // 66  // half the Y side-side width of robot
+#define robot_side_vec(dx) vec3(dx,robot_side2side,0)
 
 const static marker_info_t marker_info[]={
 	{-1,14.5}, // fallback default case
 	
-	{13, 25.0, 0, -robot_side_vec(23) }, // creeper (on 30cm full-size test plate)
-	{17, 14.5, 0, -robot_side_vec(0) }, // bird (on 20cm half-size test plate)
-	{14, 14.5, 0, -robot_side_vec(0) }, // cat
-	{25, 10.0, 0, -robot_side_vec(-15) }, // bleachers 
-	{16,  4.0, 0, -robot_side_vec(-4) }, // elipsis (on tiny 5cm test plate)
+	{13, 25.0, 1, -robot_side_vec(17) }, // creeper (on 30cm full-size test plate)
+	{17, 14.5, 1, -robot_side_vec(0) }, // bird (on 20cm half-size test plate)
+	{14, 14.5, 1, -robot_side_vec(-6) }, // cat
+	{25, 10.0, 1, -robot_side_vec(-20) }, // bleachers 
+	{16,  4.0, 1, -robot_side_vec(-4) }, // elipsis (on tiny 5cm test plate)
 	
-	{ 6, 18.0, 2, -vec3(-robot_frontback,robot_side2side-16.5,10) }, // back right plate
-	{ 7,  5.0, 2, -vec3(-robot_frontback,robot_side2side-4.5,4.5) }, 
+	{ 6, 18.0, 2, +vec3(robot_side2side-2-9,-robot_frontback,10) }, // back right plate
+	{ 7,  5.0, 2, +vec3(robot_side2side-2-18-5,-robot_frontback,4.5) }, 
   
-	{26, 18.0, 2, -vec3(robot_frontback,robot_side2side-16.5,10) }, // front right plate
-	{27,  5.0, 2, -vec3(robot_frontback,robot_side2side-4.5,4.5) }, 
-	
-	
+	{26, 18.0, 3, -vec3(robot_side2side-2-9,robot_frontback,10) }, // front right plate
+	{27,  5.0, 3, -vec3(robot_side2side-2-18-5,robot_frontback,4.5) }, 
 	
 };
 
