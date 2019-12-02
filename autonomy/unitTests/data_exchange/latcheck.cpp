47d9d89deb7a5000a27f1b57f9153b5db2931c99
mperry37 <mperry37@alaska.edu>
2019-12-01 15:32:00 -0900

    feat: test program for latency check

---
+++ autonomy/unitTests/data_exchange/latcheck.cpp
@@ -0,0 +1,19 @@
+/* Write the time, in milliseconds, to this file. */
+#include <iostream>
+#include <stdio.h>
+#include "aurora/data_exchange.h"
+
+int main() {
+    typedef uint64_t millitime_t;
+    aurora::data_exchange<millitime_t> millitime("millitime.u64");
+    millitime_t previous = 0;
+	while (true) {
+	    millitime_t currenttime = millitime.read();
+        long realLat = currenttime - previous;
+        if(realLat < 0 || realLat > 2)
+            std::cout << realLat << std::endl;
+        previous = currenttime;
+	}
+	return 0;
+}
+
