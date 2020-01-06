5a9bbc7d36b51b3c820690de3f84d50164409fe2
Dr. Orion Lawlor <lawlor@alaska.edu>
2020-01-05 17:08:07 -0900

    fix: Superficial refactoring, don't plan until there's a target.

--- autonomy/pathplanner/pathplanner.cpp
+++ autonomy/pathplanner/pathplanner.cpp
@@ -11,27 +11,31 @@
 int main() {
     //Data sources need to write to, these are defined by lunatic.h for what files we will be communicating through
     MAKE_exchange_drive_commands();
+    
     //Data source needed to read from, these are defined by lunatic.h for what files we will be communicating through
     MAKE_exchange_plan_target();
     MAKE_exchange_plan_current();
     MAKE_exchange_field_drivable();
 
     while (true) {
-        aurora::robot_loc2D old2dCoord = exchange_plan_current.read();
-        aurora::robot_loc2D old3dCoord = exchange_plan_target.read();
-        const aurora::field_drivable &currField = exchange_field_drivable.read();
-        //Some logic to determine what are the dive values
-        //Currently just creates empty object needs some data?
-        aurora::drive_commands newDrive;
-        //Set
-        //spinning around in place
-        newDrive.left = 100.0;
-        newDrive.right = 0.0;
+        aurora::robot_loc2D target = exchange_plan_target.read();
+        
+        if (target.percent>=1.0) { // we have a valid target
+            aurora::robot_loc2D current = exchange_plan_current.read();
+            const aurora::field_drivable &currField = exchange_field_drivable.read();
+            
+            //Some logic to determine what are the dive values
+            //Currently just creates empty object needs some data?
+            aurora::drive_commands newDrive;
+            //Set
+            //spinning around in place
+            newDrive.left = 100.0;
+            newDrive.right = 0.0;
 
-        //writing new data to files:
-        exchange_drive_commands.write_begin() = newDrive;
-        exchange_drive_commands.write_end();
-       
+            //writing new data to files:
+            exchange_drive_commands.write_begin() = newDrive;
+            exchange_drive_commands.write_end();
+        }
 
         //Sleep? Forced latency?
         aurora::data_exchange_sleep(10);
