4d276e3d5dca2f13cade8a578df8d7abf4acb257
Dr. Orion Lawlor <lawlor@alaska.edu>
2020-01-26 13:20:20 -0900

    feat: Andretti now drives!  It's still a bit twichy, but can help avoid overshoot even with lots of planner lag.

--- autonomy/andretti/andretti.cpp
+++ autonomy/andretti/andretti.cpp
@@ -8,33 +8,101 @@
 #include "aurora/data_exchange.h"
 #include "aurora/lunatic.h"
 
-//TODO: Write logic for how andretti will drive us!?
+bool verbose=true;
+
+// cap turning power
+float turncap(float turnpower) {
+    const float cap=1.0;
+    if (turnpower>+cap) return +cap;
+    if (turnpower<-cap) return -cap;
+    return turnpower;
+}
+
+/*
+  Find the drive commands to use when at this point along this path.
+*/
+aurora::drive_commands path_to_drive(const aurora::path_plan &path,
+    const aurora::robot_loc2D &cur)
+{
+    aurora::drive_commands drive;
+    drive.left = drive.right = 0.0;
+    int best_index = -1;
+    double best_distance=20.0; //<- cm distance--if nobody's closer than this, just stop and wait
+    const double angle_to_cm=0.5; // scales angle mismatch (deg) to position mismatch (cm)
+    
+    for(unsigned short i = 0; i < path.plan_len-1; i++)
+    {
+        aurora::robot_loc2D step = path.path_plan[i];
+        double distance = 
+           length(step.center()-cur.center())
+           + angle_to_cm*step.angle_diff(step.angle,cur.angle);
+        if (distance < best_distance) {
+            best_distance = distance;
+            best_index = i;
+        }
+    }
+    if (verbose) printf("andretti: index %2d matches with distance %.1f\n", best_index,best_distance);
+    
+    // Figure out the drive commands to follow the path from this position
+    if (best_index>=0) 
+    { 
+        aurora::robot_loc2D step = path.path_plan[best_index];
+        aurora::robot_loc2D next = path.path_plan[best_index+1];
+        vec2 move=next.center()-step.center(); //<- fixme: should this be next - cur?
+        if (length(move)>0.01) move=normalize(move); // we just want the drive direction
+        float forward = dot(step.forward(),move);
+        float turn = next.angle-cur.angle; if (turn>180.0) turn-=360.0;
+        const double turnscale = 0.1; // degrees to 0-1 drive power (steering agressiveness)
+        
+        const float   autonomous_speed=15.0f; //<- fixme: put this scale factor into backend
+        drive.left  = autonomous_speed * (forward - turncap(turnscale*turn));
+        drive.right = autonomous_speed * (forward + turncap(turnscale*turn));
+        if (verbose) {
+            printf("   fw %.2f  turn %.2f     ",forward,turn); drive.print();
+            /*
+            printf("      "); cur.print(stdout," =cur\n"); 
+            printf("         "); step.print(stdout," =step\n"); 
+            printf("             "); next.print(stdout," =next\n");
+            */
+        }
+        
+    }
+    return drive;
+}
 
 int main()
 {
+
     MAKE_exchange_drive_commands();
     MAKE_exchange_plan_current();
     MAKE_exchange_path_plan();
-    aurora::drive_commands newDrive;
+    
+    aurora::robot_loc2D cur = exchange_plan_current.read();
+    aurora::path_plan path = exchange_path_plan.read();
+    bool updated=true;
     while (true) {
-        aurora::path_plan currentPlan;
-        aurora::robot_loc2D current = exchange_plan_current.read();
         if(exchange_path_plan.updated()){
-            currentPlan = exchange_path_plan.read();
+            path = exchange_path_plan.read();
+            updated=true;
         }
-        while(!exchange_path_plan.updated()){
-            for(unsigned short i = 0; i < currentPlan.plan_len; i++)
-            {
-                aurora::robot_loc2D nextpos = currentPlan.path_plan[i];
-                newDrive.left = newDrive.right = 0.0;
-                
-            }
+        if(exchange_plan_current.updated()){
+            cur = exchange_plan_current.read();
+            updated=true;
         }
         
-        //Logic here for this stuff to make a new drive command and 
-        exchange_drive_commands.write_begin() = newDrive;
-        exchange_drive_commands.write_end();
+        if (updated) {
+            updated=false;
+            
+            // Figure out the step of the path most similar to our current position
+            aurora::drive_commands drive = path_to_drive(path,cur);
+            
+            exchange_drive_commands.write_begin() = drive;
+            exchange_drive_commands.write_end();
+        
+        }
 
+        // Limit our cycle rate (to save CPU)
+        aurora::data_exchange_sleep(50);
     }
 
     return 0;
