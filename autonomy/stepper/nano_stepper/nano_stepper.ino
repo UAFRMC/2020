e9e26be936f6e9b5b3862e43a3002bdc59b073cf
Addeline Mitchell <amitchell@alaska.edu>
2020-02-16 16:17:38 -0900

    Updates unit conversion functions

--- autonomy/stepper/nano_stepper/nano_stepper.ino
+++ autonomy/stepper/nano_stepper/nano_stepper.ino
@@ -1,70 +1,87 @@
 #include <AccelStepper.h>
 #include <MultiStepper.h>
 
-#define STEPS_PER_MREV 32 // what are the specs?
-#define STEPS_PER_OREV 32 * 64 // 2048; what are the specs?
+#define STEPS_PER_MREV 200 // motor revolution; what are the specs?
+#define STEPS_PER_OREV 600 // output revolution; what are the specs?
 
-AccelStepper myStep(AccelStepper::FULL2WIRE, 2, 3);
+AccelStepper myStep(AccelStepper::DRIVER, 2, 3);
 
 void setup()
 {
   Serial.begin(115200);
   Serial.print("<Arduino Nano Ready>#");
-  myStep.setMaxSpeed(300.0);
-  myStep.setAcceleration(100.0);
-  myStep.runToNewPosition(500);
+  myStep.setMaxSpeed(150.0);
+  myStep.setAcceleration(10.0);
+  myStep.moveTo(0);
+  while(true)
+  {
+    myStep.runToNewPosition(150);
+    delay(1000);
+    myStep.runToNewPosition(0);
+    delay(1000);
+  }
   delay(500);
 }
 
-int deg2step(float deg)
+long deg2step(long degs)
 {
-  int steps = STEPS_PER_OREV * 360 / deg;
+  long steps = STEPS_PER_OREV * degs / 360;
   return steps;
 }
 
-float step2deg(int steps) // rounds down to whole degree
+long step2deg(long steps) // rounds down to whole degree
 {
-  float degs = STEPS_PER_OREV * 360 / steps;
+  long degs = steps * 360 / STEPS_PER_OREV;
   return degs;
 }
 
-float stepperComm(float ang)
+long stepperComm(long ang)
 {
-  int loc = deg2step(ang);
+  long loc = deg2step(ang);
   
   myStep.runToNewPosition(loc); // blocking
-  delay(1000); // needed?
-  
-  float current = step2deg(myStep.currentPosition());
+  //delay(1000); // needed?
+
+  Serial.print("\nCurrent Position: ");
+  Serial.println(myStep.currentPosition());
+  long current = step2deg(myStep.currentPosition());
   delay(500);
   
   return current;
 }
 
+long newAng = 0.0, curAng;
+
 void loop()
 {
-  String str, res;
-  float newAng, curAng;
+  //String str, res;
+  //long newAng, curAng;
+
+  newAng += 10.0;
   
-  while(Serial.available() > 0)
-  {
-    str = Serial.readString();
+  //while(Serial.available() > 0)
+  //{
+  //  str = Serial.readString();
 
-    if(str.length() > 0)
-    {
-      Serial.print("RECEIVED: New Angle (" + str + "°)#");
+  //  if(str.length() > 0)
+  //  {
+  //    Serial.print("RECEIVED: New Angle (" + str + "°)#");
 
-      newAng = str.toFloat();
+  //    newAng = str.toLong();
 
-      if(newAng != step2deg(myStep.currentPosition()))
-      {
+  //    if(newAng != step2deg(myStep.currentPosition()))
+  //    {
+        Serial.print("\nTold stepper to move: ");
+        Serial.println(newAng);
         curAng = stepperComm(newAng);
       
-        Serial.print("CONFIRM: Curr Angle (");
-        Serial.print(curAng, 2); Serial.print("°)#");
-      }
-      
-      str = "";
-    }
-  }
+  //      Serial.print("CONFIRM: Curr Angle (");
+  //      Serial.print(curAng, 2); Serial.print("°)#");
+        Serial.print("\nShould have moved: ");
+        Serial.println(curAng);
+  //    }
+  //    
+  //    str = "";
+  //  }
+  //}
 }
